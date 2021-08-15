ac6cf2c23f331f6249a147c0e43ac34dacb29e4d
Sulistyo Ardani <sulistyoardani@Sulistyos-MacBook-Pro.local>
2021-08-15 22:35:10 +0700

    feat: footer

--- src/components/landingPage/index.jsx
+++ src/components/landingPage/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Section1 from 'components/landingPage/parts/section1';
 import Section2 from 'components/landingPage/parts/section2';
 import Section3 from 'components/landingPage/parts/section3';
+import Footer from 'components/landingPage/parts/footer';
 
 function index() {
     return (
@@ -12,6 +13,9 @@ function index() {
             <Section2 />
             <div className="landing__container">
                 <Section3 />
+            </div>
+            <div className="landing__container">
+                <Footer />
             </div>
         </div>
     );
--- src/components/landingPage/parts/footer.jsx
+++ src/components/landingPage/parts/footer.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 
 function footer() {
     return (
-        <div>
-            <div className="df">
-                <h1 className="" />
+        <>
+            <div className="footer">
+                <div className="footer__wrap-icons">
+                    <a href="https://id.linkedin.com/in/sulistyo-ardani-a30059179">
+                        <div className="icon__fb" />
+                    </a>
+                    <a href="https://id.linkedin.com/in/sulistyo-ardani-a30059179">
+                        <div className="icon__ig" />
+                    </a>
+                    <a href="https://id.linkedin.com/in/sulistyo-ardani-a30059179">
+                        <div className="icon__twitter" />
+                    </a>
+                    <a href="https://id.linkedin.com/in/sulistyo-ardani-a30059179">
+                        <div className="icon__linkedin" />
+                    </a>
+                </div>
             </div>
-        </div>
+            <div className="copyright d-block">
+                Copyright ©2020 All rights reserved
+            </div>
+        </>
     );
 }
 
