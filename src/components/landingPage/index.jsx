1e775ee337fc8011cb87052a97e801cbd3b70246
Sulistyo Ardani <sulistyoardani@Sulistyos-MacBook-Pro.local>
2021-08-22 19:44:16 +0700

    sdsd

--- src/components/landingPage/index.jsx
+++ src/components/landingPage/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Section1 from 'components/landingPage/parts/section1';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Section2 from 'components/landingPage/parts/section2';
 import Section3 from 'components/landingPage/parts/section3';
 import Footer from 'components/landingPage/parts/footer';
@@ -21,6 +22,9 @@ function index() {
         </div>
     );
 =======
+=======
+import Section2 from 'components/landingPage/parts/section2';
+>>>>>>> d669850 (feat: section 2)
 
 function index() {
   return (
@@ -28,6 +32,7 @@ function index() {
       <div className="landing__container">
         <Section1 />
       </div>
+      <Section2 />
     </div>
   );
 >>>>>>> 84a14fc (feat: section1)
--- src/components/landingPage/parts/section2.jsx
+++ src/components/landingPage/parts/section2.jsx
@@ -5,6 +5,7 @@ import Loc from 'assets/img/icons/location.svg';
 import Email from 'assets/img/icons/email.svg';
 
 const section2 = () => (
+<<<<<<< HEAD
     <div className="section-2">
         <div className="section-2__container">
             <div className="landing__container d-md-flex d-block text-left wrapping-pad">
@@ -73,6 +74,42 @@ const section2 = () => (
             </div>
         </div>
     </div>
+=======
+  <div className="section-2">
+    <div className="section-2__container">
+      <div className="landing__container d-flex text-left wrapping-pad">
+        <div className="col-md-6">
+          <div className="title">
+            CONTACT
+          </div>
+          <ul>
+            <li>
+              <img src={Telp} alt="" />
+              <span>84 - 969877097</span>
+            </li>
+            <li>
+              <img src={Email} alt="" />
+              <span>toannd.figmateam@gmail.com</span>
+            </li>
+            <li>
+              <img src={Calc} alt="" />
+              <span>Lita Co, Ltd.999 Conway Blvd. </span>
+            </li>
+            <li>
+              <img src={Loc} alt="" />
+              <span>California 90999, United States</span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-md-6">
+          <div className="title">
+            EDUCATION
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+>>>>>>> d669850 (feat: section 2)
 );
 
 export default section2;
